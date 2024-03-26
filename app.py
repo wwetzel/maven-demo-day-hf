119c76cf2090cffa2b473c2ba46b7dd0e20fb3ec
Amanda.VanderWal <amanda.vanderwal@kiewit.com>
2024-03-26 18:06:22 +0000

    updates

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # You can find this code for Chainlit python streaming here (https://docs.chainlit.io/concepts/streaming/python)
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # OpenAI Chat completion
@@ -34,9 +35,14 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts.chat import (
     ChatPromptTemplate,
     HumanMessagePromptTemplate,
-    MessagesPlaceholder
+    MessagesPlaceholder,
 )
-
+from langchain.chains.query_constructor.base import (
+    StructuredQueryOutputParser,
+    get_query_constructor_prompt,
+    load_query_constructor_runnable,
+)
+from langchain.retrievers.self_query.chroma import ChromaTranslator
 from langchain_experimental.tools import PythonREPLTool
 
 from langchain_openai import OpenAIEmbeddings
@@ -46,7 +52,7 @@ from utils import read_from_sqlite, load_sqlite
 
 db_uri = "sqlite:///hr_database.db"
 # db_uri_ro = "file:hr_database.db?mode=ro"
-data_fp = os.getenv('DATA_FP')# '/opt/ddlfiles/KDS_DEV/DATA/DS/maven/'
+data_fp = os.getenv("DATA_FP")  # '/opt/ddlfiles/KDS_DEV/DATA/DS/maven/'
 hr_fn = "maven_final_synthetic_data.xlsx"
 # print('LOADING SQLITE')
 hr_df = read_from_sqlite(db_uri)
@@ -59,57 +65,67 @@ repl_tool = Tool(
 )
 tool_description = "Use this tool to gather numeric data and averages"
 agent_db = SQLDatabase.from_uri(db_uri)
-sql_toolkit = SQLDatabaseToolkit(db=agent_db, llm=ChatOpenAI(temperature=0), tool_description = tool_description)
+sql_toolkit = SQLDatabaseToolkit(
+    db=agent_db, llm=ChatOpenAI(temperature=0), tool_description=tool_description
+)
 sql_context = sql_toolkit.get_context()
 sql_tools = sql_toolkit.get_tools()
 
 messages = [
     HumanMessagePromptTemplate.from_template("{input}"),
     # AIMessage(content=SQL_FUNCTIONS_SUFFIX),
-    AIMessage(""" Use search_survey_response to summarize exit survey data. Use the sql tool for quantitative questions by converting natural language to a sql query."""),
+    AIMessage(
+        """ Use search_survey_response to summarize exit survey data. Use the sql tool for quantitative questions by converting natural language to a sql query."""
+    ),
     MessagesPlaceholder(variable_name="agent_scratchpad"),
 ]
 
 prompt = ChatPromptTemplate.from_messages(messages)
 
 if os.path.exists("./chroma_db"):
-    print('LOADING CHROMA CACHE')
-    vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=OpenAIEmbeddings())
+    print("LOADING CHROMA CACHE")
+    vectorstore = Chroma(
+        persist_directory="./chroma_db", embedding_function=OpenAIEmbeddings()
+    )
 else:
-    print('LOADING CHROMA DOCUMENTS')
-    documents = DataFrameLoader(hr_df, page_content_column="main_quit_reason_text").load()
-    print('EMBEDDING CHROMA DOCUMENTS')
-    vectorstore = Chroma.from_documents(documents, OpenAIEmbeddings(), persist_directory="./chroma_db")
+    print("LOADING CHROMA DOCUMENTS")
+    documents = DataFrameLoader(
+        hr_df, page_content_column="main_quit_reason_text"
+    ).load()
+    print("EMBEDDING CHROMA DOCUMENTS")
+    vectorstore = Chroma.from_documents(
+        documents, OpenAIEmbeddings(), persist_directory="./chroma_db"
+    )
 
 metadata_field_info = [
     AttributeInfo(
         name="term_year",
-        description="The year the employee quit or was terminated, year from 2019 to 2024",
+        description="The year the employee quit or was terminated, year from 2019 to 2024. NOTE: Only use the 'eq' operator if a specifc year is mentioned",
         type="integer",
     ),
     AttributeInfo(
         name="term_month",
-        description="The month the employee quit or was terminated, month from 1 to 12",
+        description="The month the employee quit or was terminated, month from 1 to 12. NOTE: Only use the 'eq' operator if a specifc month is mentioned",
         type="integer",
     ),
     AttributeInfo(
         name="job_title",
-        description="The job title of the terminated employee, one of superintendent 1, superintendent 2, design engineer, field engineer 1, field engineer 2, project manager 1, superintendent 1, superintendent 2",
+        description=f"The job title of the terminated employee, one of {hr_df['job_title'].unique().tolist()}. NOTE: the query will be case sensitive, so match the options.",
         type="string",
     ),
     AttributeInfo(
         name="business_unit",
-        description="The department or business unit the terminated employee was located in, one of business unit A, business unit B, business unit C, business unit D, business unit E",
+        description=f"The department or business unit the terminated employee was located in, one of {hr_df['business_unit'].unique().tolist()}. NOTE: the query will be case sensitive, so match the options.",
         type="string",
     ),
     AttributeInfo(
-        name="Gender",
+        name="gender",
         description="The gender of the terminated employee, one of male or female",
         type="string",
     ),
     AttributeInfo(
         name="main_quit_reason_text_sentiment",
-        description="The sentiment of the main quit reason, one of Very Postive, Positive, Neutral, Negative, Very Negative",
+        description=f"The sentiment of the main quit reason, one of {hr_df['main_quit_reason_text_sentiment'].unique().tolist()}. NOTE: the query will be case sensitive, so match the options.",
         type="string",
     ),
     AttributeInfo(
@@ -118,37 +134,45 @@ metadata_field_info = [
         type="integer",
     ),
 ]
-
-document_content_description = "The exit survery or reason for employees leaving the company"
+document_content_description = "Reason employee is leaving the company"
 
 llm = ChatOpenAI(model="gpt-4-0613", temperature=0)
-retriever = SelfQueryRetriever.from_llm(
-    llm,
-    vectorstore,
+
+
+query_prompt = get_query_constructor_prompt(
     document_content_description,
     metadata_field_info,
-    enable_limit = True,
+)
+output_parser = StructuredQueryOutputParser.from_components()
+query_constructor = query_prompt | llm | output_parser
+retriever = SelfQueryRetriever(
+    query_constructor=query_constructor,
+    vectorstore=vectorstore,
+    structured_query_translator=ChromaTranslator(),
+    enable_limit=True,
     search_kwargs={"k": 50},
-    verbose = True
+    verbose=True,
 )
 
-chain = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', retriever=retriever, return_source_documents=True, verbose=True, input_key="question")
-retriever_tool = Tool(
-            name="survey_search",
-            func=lambda query: chain({"question": query}),
-            description="Useful for when you need to answer questions about summarizing exit survey responses. Input should be a fully formed question."
-        )
+retriever_tool = create_retriever_tool(
+    retriever,
+    name="survey_search",
+    description="Use this tool to summarize semantic information on why employees are quitting the company",
+)
 
 openai_llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-openai_agent = create_openai_tools_agent(openai_llm, sql_tools + [repl_tool] + [retriever_tool], prompt)
+openai_agent = create_openai_tools_agent(
+    openai_llm, sql_tools + [repl_tool] + [retriever_tool], prompt
+)
 
 agent_executor = AgentExecutor(
     agent=openai_agent,
-    tools = sql_tools + [repl_tool] + [retriever_tool],
+    tools=sql_tools + [repl_tool] + [retriever_tool],
     verbose=True,
     return_intermediate_steps=True,
-    max_iterations=10
+    max_iterations=10,
 )
+
 
 @cl.on_chat_start  # marks a function that will be executed at the start of a user session
 async def start_chat():
@@ -163,6 +187,7 @@ async def start_chat():
 
     cl.user_session.set("settings", settings)
 
+
 @cl.on_message
 async def main(message: cl.Message):
     # settings = cl.user_session.get("settings")
@@ -170,8 +195,7 @@ async def main(message: cl.Message):
     question = message.content
 
     response = agent_executor.invoke({"input": question})
-    response_content = response['output']
+    response_content = response["output"]
 
     msg = cl.Message(content=response_content)
     await msg.send()
-
